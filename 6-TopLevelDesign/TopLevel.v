3d8feaf157ada880825c9ca20340a4bd19a14a8f
CougarBellinger <cougarbellinger@gmail.com>
2024-09-09 16:08:51 -0700

    Created top level

---
+++ 6-TopLevelDesign/TopLevel.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// ECE369A - Computer Architecture
+// Laboratory 
+// Module - TopLevel
+// Description - Implementation of the top level design given in the powerpoint slides
+////////////////////////////////////////////////////////////////////////////////
+
+module TopLevel(Clk, Reset)
+
+input Clk, Reset;
+
+//module ClkDiv(Clk, Rst, ClkOut);
+//module InstructionFetchUnit(Instruction, PCResult, Reset, Clk);
+//module Two4DigitDisplay(Clk, NumberA, NumberB, out7, en_out);
+
+
+endmodule
