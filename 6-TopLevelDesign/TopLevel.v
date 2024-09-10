defc67adf909cd8e6d66ec0b6cf50be4d6b60cc0
Jake <Jcortez96@hotmail.com>
2024-09-09 20:22:47 -0700

    Added modules to top level

--- 6-TopLevelDesign/TopLevel.v
+++ 6-TopLevelDesign/TopLevel.v
@@ -12,8 +12,11 @@ module TopLevel(Clk, Reset)
 input Clk, Reset;
 
 //module ClkDiv(Clk, Rst, ClkOut);
+ClkDiv Clk100(Clk, Reset)
 //module InstructionFetchUnit(Instruction, PCResult, Reset, Clk);
+InstructionFetchUnit FetchUnit(Instruction, PCResult, Reset, Clk);
 //module Two4DigitDisplay(Clk, NumberA, NumberB, out7, en_out);
+Two4DigitDisplay Display(Clk, Instruction[15:0], PCResult[15:0], out7, en_out);
 
 
 endmodule
