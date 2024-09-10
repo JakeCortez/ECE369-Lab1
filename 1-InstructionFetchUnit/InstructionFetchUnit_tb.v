cb648de63d5a8ce66a6e3cf749a1990948e0a0c0
Jake <Jcortez96@hotmail.com>
2024-09-09 20:42:10 -0700

    More Top level fixes and fetch unit testbench

--- 1-InstructionFetchUnit/InstructionFetchUnit_tb.v
+++ 1-InstructionFetchUnit/InstructionFetchUnit_tb.v
@@ -1,6 +1,36 @@
 `timescale 1ns / 1ps
 
-module InstructionFetchUnit_tb()
+module InstructionFetchUnit_tb(); 
 
+    wire [31:0] Instruction;
+    wire [31:0] PCResult;
+    reg Clk;
+    reg Reset;
+//module InstructionFetchUnit(Instruction, PCResult, Reset, Clk);
+
+	InstructionFetchUnit u0(
+        .Instruction(Instruction),
+        .PCResult(PCResult),
+        .Reset(Reset),
+        .Clk(Clk)
+	);
+
+    initial begin
+		Clk <= 1'b0;
+		forever #10 Clk <= ~Clk;
+	end
+
+
+	initial begin
+	
+    /* Please fill in the implementation here... */
+	
+		reset = 1;
+        #20;
+        reset = 0;
+        #20;
+        //$finish;
+
+	end
 
 endmodule
--- 6-TopLevelDesign/TopLevel.v
+++ 6-TopLevelDesign/TopLevel.v
@@ -12,9 +12,9 @@ module TopLevel(Clk, Reset)
 input Clk, Reset;
 
 //module ClkDiv(Clk, Rst, ClkOut);
-ClkDiv Clk100(Clk, Reset)
+ClkDiv Clk100(Clk, 0, ClkOut);
 //module InstructionFetchUnit(Instruction, PCResult, Reset, Clk);
-InstructionFetchUnit FetchUnit(Instruction, PCResult, Reset, Clk);
+InstructionFetchUnit FetchUnit(Instruction, PCResult, Reset, ClkOut);
 //module Two4DigitDisplay(Clk, NumberA, NumberB, out7, en_out);
 Two4DigitDisplay Display(Clk, Instruction[15:0], PCResult[15:0], out7, en_out);
 
